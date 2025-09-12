e041ac9eee46ab925e26dc5578f286a6084541b7
Anurag Kanwar <aigkaraig@gmail.com>
2025-09-13 00:31:43 +0530

    full working done

--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "drizzle-kit";
 
 export default {
-  schema: "./src/db/schemas/index.ts",
+  schema: "./src/db/index.ts",
   out: "./migrations",
   dbCredentials: {
     url: process.env.TURSO_DATABASE_URL ?? "http://127.0.0.1:8080",
